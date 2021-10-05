070fba29ad706ca4e20a6e01b6d022e773e0ad2f
mbkoltai <kolmisi@gmail.com>
2021-10-05 23:36:08 +0100

    cleanup

--- command_arg_test.R
+++
@@ -1,7 +0,0 @@
-# command arg test
-
-k_start_end <- as.numeric(commandArgs(trailingOnly=TRUE))[1:2]
-filename <- commandArgs(trailingOnly=TRUE)[3]
-
-print(paste0(c("k_start_end:",k_start_end),collapse = " "))
-print(filename)
