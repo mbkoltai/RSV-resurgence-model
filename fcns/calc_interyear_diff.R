0dea1eb361d7911cdfaa0edd0ec8075cce3307bc
mbkoltai <kolmisi@gmail.com>
2021-11-01 00:57:44 +0000

    print name of file being processed

--- fcns/calc_interyear_diff.R
+++ fcns/calc_interyear_diff.R
@@ -7,15 +7,17 @@ foldername<-commandArgs(trailingOnly=TRUE)[1]
 file_list <- list.files(path=foldername,pattern="dyn_parsets*")
 start_date_dyn_save <- commandArgs(trailingOnly=TRUE)[2]
 yday_start_end<-yday(as.Date(commandArgs(trailingOnly=TRUE)[3])); print(yday_start_end)
-for(k_par in 1:length(file_list)){
-  dyn_df <- read_csv(paste0(foldername,file_list[k_par])); print(file_list[k_par])
-  x <- dyn_df %>% mutate(date=as.Date(start_date_dyn_save)+t-min(t)) %>% 
+for(k_file in 1:length(file_list)){
+  dyn_df <- read_csv(paste0(foldername,file_list[k_file])); print(file_list[k_file])
+  for (k_par in unique(dyn_df$par_id)) {
+  x <- dyn_df %>% filter(par_id==k_par) %>% mutate(date=as.Date(start_date_dyn_save)+t-min(t)) %>% 
     mutate(day_of_year=yday(date),epi_year=ifelse(day_of_year>=yday_start_end,paste0(year(date),"_",year(date)+1),
-                    paste0(year(date)-1,"_",year(date))) ); print("created cols")
-  x <- x %>% group_by(agegroup,infection,par_id,day_of_year) %>% 
+                    paste0(year(date)-1,"_",year(date))) ) %>% group_by(agegroup,infection,par_id,day_of_year) %>% 
     summarise(diff_interyr=abs(diff(value)),value=mean(value)) %>% group_by(agegroup,infection,par_id) %>% 
     summarise(cumul_mean_incid=sum(value),sum_abs_diff=sum(diff_interyr),sum_rel_diff=sum(diff_interyr)/sum(value))
-  print("done, param: ",k_par)
-  write_csv(x,paste0(foldername,"summ_diff_interyr.csv"),append=ifelse(k_par==1,F,T))
-  # if (k_par %% 10 == 0) {     print(k_par) }
+  print(paste0("done, file: ",k_file, ", param: ", k_par))
+  write_csv(x,paste0(foldername,"summ_diff_interyr.csv"),
+            append=ifelse(k_file==1 & k_par==unique(dyn_df$par_id)[1],F,T))
+  }
+  # if (k_file %% 10 == 0) {     print(k_file) }
 }
