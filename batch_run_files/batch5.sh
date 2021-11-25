fc899deaf52bc7deaf3900da6b7a5d669a8583e2
mbkoltai <kolmisi@gmail.com>
2021-11-25 17:29:39 +0000

    correct write_run_file

--- batch_run_files/batch5.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash 
-module load R/3.6.3
-Rscript --vanilla fcns/parscan_runner_cmd_line.R 1460 1823 25 4 partable_full.csv data/estim_attack_rates.csv nosave 2018-09-01 > simul_output/parscan/parallel/nohup_5.out 
--- fcns/write_run_file.R
+++ fcns/write_run_file.R
@@ -1,17 +1,16 @@
 # nohup Rscript --vanilla --verbose my_script.R 1 5 1 5 &> nohup_1.out &
 arg_vals=as.numeric(commandArgs(trailingOnly=TRUE)[1:4])
 n_core=arg_vals[1]; n_row=arg_vals[2]; simul_dur=arg_vals[3]; post_npi=arg_vals[4]
-partable_filename <- commandArgs(trailingOnly=TRUE)[5]; estim_attack_rate_filename <- commandArgs(trailingOnly=TRUE)[6]
-saveflag <- commandArgs(trailingOnly=TRUE)[7]; sep_flag<-commandArgs(trailingOnly=TRUE)[8]
-start_date_dyn_save <- commandArgs(trailingOnly=TRUE)[9]
-memory_max <- commandArgs(trailingOnly=TRUE)[10]
+partable_filename <- commandArgs(trailingOnly=TRUE)[5]; # estim_attack_rate_filename <- commandArgs(trailingOnly=TRUE)[6]
+saveflag <- commandArgs(trailingOnly=TRUE)[6]; sep_flag<-commandArgs(trailingOnly=TRUE)[7]
+start_date_dyn_save <- commandArgs(trailingOnly=TRUE)[8]
+memory_max <- commandArgs(trailingOnly=TRUE)[9]
 parscan_split <- lapply(1:n_core,function(x) matrix(sort(c(round(seq(1,n_row,length.out=n_core+1)),
                                 round(seq(1,n_row,length.out=n_core+1))[2:n_core]+1)),nrow=2)[,x])
 
 command_list <- unlist(lapply(1:n_core, # "#!/bin/bash\n",
   function(x) paste0("nohup Rscript --vanilla fcns/parscan_runner_cmd_line.R ",
- paste0(c(parscan_split[[x]],simul_dur,post_npi,partable_filename,estim_attack_rate_filename,saveflag,
-          start_date_dyn_save),collapse=" "),
+ paste0(c(parscan_split[[x]],simul_dur,post_npi,partable_filename,saveflag,start_date_dyn_save),collapse=" "),
  " > simul_output/parscan/parallel/nohup_",x,".out",ifelse(x<n_core," & \n",""),collapse="") ))
 
 if (grepl("sep",sep_flag)) {
