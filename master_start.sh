f3145607499a6d99b0e98fe84f89d9032bfc1762
mbkoltai <kolmisi@gmail.com>
2021-12-02 14:09:33 +0000

    master_start.sh file

---
+++ master_start.sh
@@ -0,0 +1,10 @@
+# master start file
+#!/usr/bin/bash 
+scp batch_run_files/start_batches.sh .
+scp batch_run_files/batch*.sh .
+module load R/3.6.3
+sh start_batches.sh
+rm batch*.sh
+rm batch*.e*
+rm batch*.o*
+rm start_batches.sh
