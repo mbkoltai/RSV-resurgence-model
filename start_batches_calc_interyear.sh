3c3f258c0999bb50a7d9cbaeba84902280fc51c1
mbkoltai <kolmisi@gmail.com>
2021-12-04 00:55:39 +0000

    cleanup main folder

--- start_batches_calc_interyear.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#$ -N ARRAY_TEST_JOB
-#$ -cwd -V
-#$ -q short.q
-#$ -l mem_free=1G,h_vmem=4G
-#$ -t 1-64
-Rscript fcns/calc_interyear_diff.R  simul_output/parscan/parsets_filtered_1084_90pct_red/ 2018-09-01 2018-10-10 2020-03-15 42 9 ${SGE_TASK_ID} > simul_output/parscan/parsets_filtered_1084_90pct_red/nohup_interyear${SGE_TASK_ID}.out
