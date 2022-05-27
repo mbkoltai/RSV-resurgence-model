67080ae2c37356ead3032d53b5e16ee3d3d9c6ae
mbkoltai <kolmisi@gmail.com>
2022-05-27 12:02:40 +0100

    refresh gitignore

--- reproduce_results_revisions.R
+++ reproduce_results_revisions.R
@@ -305,7 +305,7 @@ all_dynamics_accepted <- all_dynamics_accepted %>% mutate(date=as.Date(start_dat
 all_dynamics_rejected <- bind_rows(lapply(
   list.files("simul_output/2e4_parsets/",pattern="dyn_parsets",full.names=T),
   function(x) read_csv(x) %>% filter(!par_id %in% unique(results_summ_all_reg_dyn$par_id)) %>%
-    filter(par_id %in% sample(par_id,16)) ))
+    filter(par_id %in% sample(par_id,sample(c(16,17),1))) ))
 write_csv(all_dynamics_rejected,"simul_output/2e4_parsets/all_dynamics_rejected_1e3.csv")
 all_dynamics_rejected <- all_dynamics_rejected %>% mutate(date=as.Date(start_date_dyn_save)+t-min(t)) %>% 
   filter(date<=as.Date("2020-05-11"))
@@ -316,21 +316,18 @@ SARIwatch_RSVhosp_under5_2018_2020_weekly_counts <-
   mutate(wk_n=gsub("\\.","-W",wk_n),wk_n=factor(wk_n,levels=unique(wk_n)),
          date=ISOweek2date(paste0(gsub("\\.","-W",wk_n),"-1")))
 # compare SARI-Watch with literature estimates 
-# annual RATE of hospitalisations: 500/1e5 (18-19), 494/1e5
+# annual RATE of hospitalisations per 100K population: 500/1e5 (18-19), 494/1e5
 SARIwatch_RSVhosp_under5_2018_2020_weekly_counts %>% group_by(year) %>% 
   summarise(annual_cumul_rate=sum(rate_under5yrs))
 
 # under-reporting for under-5 hospitalisations
 # annual RATE of hospitalisations from Reeves 2017: <1y: 35.1/1000, 1-4y: 5.31/1000 = 1083.8/1e5
 # annual RATE from Taylor 2016: <0.5y: 4184/1e5, 6-23mts: 1272/1e5, 2-4y: 114/1e5 = 822.7/1e5
-reported_hosp_rate_per_100k <- c(reeves_2017=(35.1*sum(rsv_age_groups$value[1:2]) + 
+reported_hosp_rate_per_100k <- c( reeves_2017=(35.1*sum(rsv_age_groups$value[1:2]) + 
                                     5.31*sum(rsv_age_groups$value[3:7]))*100/sum(rsv_age_groups$value[1:7]),
-taylor_2016=(4184*sum(rsv_age_groups$value[1])+1272*sum(rsv_age_groups$value[2:4]) + 
-               114*sum(rsv_age_groups$value[5:7]))/sum(rsv_age_groups$value[1:7]))
-
-
-under_report_factor_under5 = mean(
-  (SARIwatch_RSVhosp_under5_2018_2020_weekly_counts %>% group_by(year) %>% 
+                                taylor_2016=(4184*sum(rsv_age_groups$value[1]) + 1272*sum(rsv_age_groups$value[2:4]) + 
+                                    114*sum(rsv_age_groups$value[5:7]))/sum(rsv_age_groups$value[1:7]) )
+under_report_factor_under5 = mean( (SARIwatch_RSVhosp_under5_2018_2020_weekly_counts %>% group_by(year) %>% 
           summarise(annual_cumul_rate=sum(rate_under5yrs)))$annual_cumul_rate)/mean(reported_hosp_rate_per_100k)
 
 # hospitalisation counts for 65+
@@ -340,15 +337,15 @@ SARIwatch_RSVhosp_over65_2018_2020_weekly_counts <-
   mutate(wk_n=gsub("-","-W",wk_n),wk_n=factor(wk_n,levels=unique(wk_n)),date=ISOweek2date(paste0(wk_n,"-1")))
 
 # plot
-SARIwatch_RSVhosp_over65_2018_2020_weekly_counts %>% pivot_longer(!c(year,wk_n,pop_AGE65PLUS,date)) %>% 
-  ggplot(aes(x=wk_n,y=value,group=1)) + geom_line() + geom_point() + facet_grid(name~year,scales="free") + 
-  xlab("") + ylab("count or rate per 100K") + theme_bw() + standard_theme
+# SARIwatch_RSVhosp_over65_2018_2020_weekly_counts %>% pivot_longer(!c(year,wk_n,pop_AGE65PLUS,date)) %>% 
+#   ggplot(aes(x=wk_n,y=value,group=1)) + geom_line() + geom_point() + facet_grid(name~year,scales="free") + 
+#   xlab("") + ylab("count or rate per 100K") + theme_bw() + standard_theme
 
-# Estimates from literature (https://bmcinfectdis.biomedcentral.com/articles/10.1186/s12879-015-1218-z/tables/3)
-# 65-74: 86/100e3 (62-101); 75+: 234/100e3 (180-291)
-# from SARI-Watch
+
+# rate per 100K from SARI-Watch
 SARIwatch_RSVhosp_over65_2018_2020_weekly_counts %>% group_by(year) %>% summarise(annual_rate=sum(rate_65yplus))
-# from lit estimate
+# Estimates from literature (Fleming 2015, https://bmcinfectdis.biomedcentral.com/articles/10.1186/s12879-015-1218-z/tables/3)
+# 65-74: 86/100e3 (62-101); 75+: 234/100e3 (180-291)
 over65_hosp_rate_100k_lit_estim = (
   sum(ons_2020_midyear_estimates_uk$value[ons_2020_midyear_estimates_uk$age %in% 65:74])*86 + 
     sum(ons_2020_midyear_estimates_uk$value[76:nrow(ons_2020_midyear_estimates_uk)])*234)/
@@ -358,7 +355,7 @@ under_report_factor_over65y <- mean((SARIwatch_RSVhosp_over65_2018_2020_weekly_c
                   group_by(year) %>% summarise(annual_rate=sum(rate_65yplus)))$annual_rate) / 
                   over65_hosp_rate_100k_lit_estim
 
-# calculate weekly hospitalisations
+# calculate weekly hospitalisations of accepted
 simul_hosp_rate <- left_join( all_dynamics_accepted %>% 
     filter( (agegroup<=3 | agegroup==5) & date>as.Date("2018-09-15")) %>%
     group_by(t,date,agegroup,par_id) %>% summarise(value=sum(value)),
