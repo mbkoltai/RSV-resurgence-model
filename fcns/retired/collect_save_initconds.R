e6cc4ed0c811c3c537626712972c40067ae6e3bd
mbkoltai <kolmisi@gmail.com>
2022-06-26 12:04:29 +0100

    refresh gitignore

--- fcns/retired/collect_save_initconds.R
+++
@@ -1,8 +0,0 @@
-# collect and save outputs for initconds
-library(readr); library(dplyr)
-parall_foldername="simul_output/parscan/parallel/"
-write_csv(bind_rows(lapply(list.files(path=parall_foldername,pattern="init_cond_start*"),
-   function(x) read_csv(paste0(parall_foldername,x)))),paste0(parall_foldername,"initconds_all.csv"))
-# write_csv(bind_rows(lapply(list.files(path=parall_foldername,pattern="dyn_parsets_start*"),
-#               function(x) read_csv(paste0(parall_foldername,x)))),path=paste0(parall_foldername,"results_dyn_all.csv"))
-# unlink(paste0(parall_foldername,list.files(path=parall_foldername,pattern="parsets_start*")))
